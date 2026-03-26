b05e0c539ea7860c239aa16690e368cd2d0beadf
MARCIO INACIO FERREIRA <inacioferreiramarcio@gmail.com.br>
2026-03-26 08:44:28 -0300

    corrigindo lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,5 +3,6 @@ module.exports = {
   "*.{js, jsx, ts, tsx}": (filenames) => [
     `prettier --write ${filenames.join(" ")}`,
     `eslint --fix ${filenames.join(" ")}`,
+    `npm test -- --findRelatedTests ${filenames.join(" ")}`,
   ],
 };
